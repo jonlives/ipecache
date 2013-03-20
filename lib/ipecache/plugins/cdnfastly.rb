b5f8c92b4bea6af8ce4248d568c560f0dacde8f8
Jon Cowie <jcowie@etsy.com>
2013-03-20 12:51:43 +0000

    Fastly plugin

---
+++ lib/ipecache/plugins/cdnfastly.rb
@@ -0,0 +1,60 @@
+require 'ipecache/plugins/plugin'
+require 'net/http'
+
+module Ipecache
+  module Plugins
+    class CDNFastly < Plugin
+      name :fastly
+      hooks :cdn_purge
+
+      def perform
+        safe_require 'uri'
+
+        api_key = config.api_key
+
+        if api_key.nil?
+          plugin_puts("Fastly API key not specified, Exiting...")
+          exit 1
+        end
+
+        plugin_puts "Using api key #{api_key}"
+        plugin_puts "Beginning URL Purge from Fastly..."
+
+        urls.each do |u|
+
+          url = u.chomp
+          hostname = URI.parse(url).host
+          path = URI.parse(url).path
+
+          http = Net::HTTP.new("api.fastly.com", "443")
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+          request = Net::HTTP::Purge.new(path)
+          request.add_field("X-Forwarded-For", "0.0.0.0")
+          request.add_field("Accept", "application/json")
+          request.add_field("User-Agent", "Ipecache")
+          request.add_field("Content-Type", "application/x-www-form-urlencoded")
+          request.add_field("X-Fastly-Key", api_key)
+          request.add_field("Host", hostname)
+
+          response = http.request(request)
+
+          if response.code.to_i != 200
+            plugin_puts "Response Code: #{response.code}"
+            plugin_puts response.body
+            exit 1
+          else
+            plugin_puts "Purge successful!"
+          end
+        end
+      end
+    end
+  end
+end
+
+class Net::HTTP::Purge < Net::HTTPRequest
+  METHOD = 'PURGE'
+  REQUEST_HAS_BODY = false
+  RESPONSE_HAS_BODY = true
+end
